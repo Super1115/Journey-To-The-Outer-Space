0c04d3830a5f32b9a69f6589841625e5dbfca98e
Super <chuangsuper@gmail.com>
2023-10-08 16:18:39 +0800

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,21 +1,24 @@
 import google.generativeai as palm
-import os
+import requests as rq
 import json
+
 
 hiddenFile = open('./json/hidden.json')
 hiddenData = json.load(hiddenFile)
-
-
-import requests as rq
-import json
 PALM_API_KEY = api_key=hiddenData["PaLMAPIKey"]
-response = rq.post(f"https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key={PALM_API_KEY}", json={
+MODEL = "models/chat-bison-001" # 模型
+METHOD = "generateMessage" # 要用模型的哪個功能
+DATA = { # 要使用這個method需要傳送的資料
     "prompt": {
-        "text": input("YOU:") # 你的訊息
-        },
-    "temperature": 0.5,
+        "text": ""  # 這邊就是放要輸入的資料!!!
+     },
+    "temperature": 1.0,
     "candidateCount": 1 # 這邊調整你想要的回覆數量
-})
+}
+response = rq.post(f"https://generativelanguage.googleapis.com/v1beta2/{MODEL}:{METHOD}?key={PALM_API_KEY}", json=DATA)
+
+# ----------- 上面是傳送資料跟取得回復 ----- 下面則是單純把回復印出來
+
 for result in response.json()["candidates"]:
     print(result["output"])
     print("-"*100)
