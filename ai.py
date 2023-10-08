2352bfeadeb5f761d96c27dc0d58014a4e341d2e
Super <chuangsuper@gmail.com>
2023-10-08 14:47:33 +0800

    change ai.py to using rest api

--- ai.py
+++ ai.py
@@ -1,156 +1,21 @@
-"""
-At the command line, only need to run once to install the package via pip:
-$ pip install google-generativeai
-"""
-
 import google.generativeai as palm
-import pprint
 import os
 import json
 
 hiddenFile = open('./json/hidden.json')
 hiddenData = json.load(hiddenFile)
-palm.configure(api_key=hiddenData["PaLMAPIKey"])
 
 
-defaults = {
-  'model': 'models/chat-bison-001',
-  'temperature': 0.05,
-  'candidate_count': 1,
-  'top_k': 1,
-  'top_p': 0.95,
-}
-context = "you are a agent from Planetary Tourism Office provide information to user about Space Travel"
-examples = [
-  [
-    "who are you",
-    "i'm a Planetary Tourism Office agent"
-  ],
-  [
-    "what is the Planetary Tourism Office",
-    " the Planetary Tourism Office is a travel agency for space travel we provide information for Space Travel"
-  ],
-  [
-    "what does the Planetary Tourism Office do",
-    "we provide information for Space Travel"
-  ],
-  [
-    "what do you do",
-    "I work at the Planetary Tourism Office,which provides information for Space Travel"
-  ],
-  [
-    "Mars",
-    "Mars is a great destination for Space Tarvel, there's lots of Tourist Attractions on Mars"
-  ],
-  [
-    "I want to go to the moon",
-    "The moon is a neer destination for Space Travel from Earth,there is also lots of Tourist Attractions on the moon"
-  ],
-  [
-    "i want to go to Earth",
-    "Good destination for Space Travel, You are already on Earth!"
-  ],
-  [
-    "The Moon",
-    "The Moon is a great destination for Space Tarvel, there's lots of Tourist Attractions on Mars"
-  ],
-  [
-    "Neptune",
-    "Neptune is the eighth planet from the Sun, there's lots of things to do on Neptune"
-  ],
-  [
-    "which Space Travel destination do you recommend",
-    " I recommend"
-  ],
-  [
-    "who are you",
-    "I'm a agent from the Planetary Tourism Office, I provide you information about Space Travel"
-  ],
-  [
-    "Talk about you",
-    "I work at the Planetary Tourism Office, which provides information for Space Travel"
-  ],
-  [
-    "Neptune",
-    "Neptune is a great destination for Space Tarvel, there's lots of Tourist Attractions on Mars"
-  ],
-  [
-    "Jupiter",
-    "Jupiter is a great destination for Space Tarvel, there's lots of Tourist Attractions on Mars"
-  ],
-  [
-    "Saturn",
-    "Saturn is a great destination for Space Tarvel, there's lots of Tourist Attractions on Mars"
-  ],
-  [
-    "The Sun",
-    "A \"hot\" Space travel destination"
-  ],
-  [
-    "Mercury",
-    "Mercury is a great destination for Space Tarvel, there's lots of Tourist Attractions on Mars"
-  ],
-  [
-    "The Sun",
-    "If you are planning a trip to The Sun"
-  ],
-  [
-    "Venus",
-    "A trip to Venus will be"
-  ],
-  [
-    "Uranus",
-    "A trip to Uranus will be"
-  ],
-  [
-    "Proxima Centauri",
-    "A trip to Proxima Centauri takes"
-  ],
-  [
-    "tell me more about Jupiter",
-    "Jupiter is the fifth planet from the Sun and also the largest in our solar system. Like all the other planets, it was likely formed in a massive, ancient cloud of gas, dust, and ice that collapsed into a spinning disc. Our Sun was born at its centre, and the planets were created about 4.5 billion years ago from particles sticking together along rings in the disc.\n\nScientists believe that many rocky planets larger than Earth existed in the early inner solar system. Physical forces moved Jupiter closer to the Sun and destroyed these other planets, allowing the formation of the current rocky planets (Mercury, Venus, Earth and Mars) before pushing Jupiter back outwards to its current position.\n\nJupiter has at least 79 known moons, which were drawn by the massive planet's strong gravitational pull. Fifty-three of them have names, while 26 are not yet named."
-  ],
-  [
-    "tell me more about Mars",
-    "Mars is the fourth planet from the Sun in our solar system. Scientists believe that all of the planets were created just over 4.5 billion years ago. The solar system began as a large cloud of gas, dust, and ice, which collapsed into a spinning disc. The Sun was formed at its centre and particles began sticking together along rings in the disc – leading to the formation of the planets."
-  ],
-  [
-    "Pluto",
-    "Pluto is a Space Travel location"
-  ],
-  [
-    "Polaris",
-    "Polaris is a Space Travel location"
-  ],
-  [
-    "Alpha Centauri",
-    "Alpha Centauri is a Space Travel location"
-  ],
-  [
-    "Betelgeuse",
-    "Betelgeuse Centauri is a Space Travel location"
-  ],
-  [
-    "Mars",
-    "Here are some of the best places to visit on Mars"
-  ],
-  [
-    "who are you",
-    "I'm a travel agency agent"
-  ],
-  [
-    "where do you work at",
-    "I work at the Planetary Tourism Office"
-  ]
-]
-messages = [
-  input("you:"),
-]
-messages.append("NEXT REQUEST")
-response = palm.chat(
-  **defaults,
-  context=context,
-  examples=examples,
-  messages=messages
-)
-print(response.last) # Response of the AI to your most recent request
+import requests as rq
+import json
+PALM_API_KEY = api_key=hiddenData["PaLMAPIKey"]
+response = rq.post(f"https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key={PALM_API_KEY}", json={
+    "prompt": {
+        "text": input("YOU:") # 你的訊息
+        },
+    "temperature": 0.5,
+    "candidateCount": 1 # 這邊調整你想要的回覆數量
+})
+for result in response.json()["candidates"]:
+    print(result["output"])
+    print("-"*100)
