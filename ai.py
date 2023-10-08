725084cd75b56a813165b62d5434749b19408761
LianTwilight <liankirito@yahoo.com.tw>
2023-10-08 16:29:51 +0800

    Integrate ai.py and app.py
    combination

--- ai.py
+++ ai.py
@@ -1,11 +1,16 @@
-import google.generativeai as palm
+from flask import Flask, render_template, request, Response
 import requests as rq
 import json
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('mainPage.html')
 
 hiddenFile = open('./json/hidden.json')
 hiddenData = json.load(hiddenFile)
-PALM_API_KEY = api_key=hiddenData["PaLMAPIKey"]
+PALM_API_KEY = "AIzaSyDvg-WzKnbYhWNY3G4WuNMnOWiG9tMRXes" # 要回復成 PALM_API_KEY = api_key=hiddenData["PaLMAPIKey"]
 MODEL = "models/chat-bison-001" # 模型
 METHOD = "generateMessage" # 要用模型的哪個功能
 DATA = { # 要使用這個method需要傳送的資料
@@ -22,3 +27,6 @@ response = rq.post(f"https://generativelanguage.googleapis.com/v1beta2/{MODEL}:{
 for result in response.json()["candidates"]:
     print(result["output"])
     print("-"*100)
+
+if __name__ == '__main__':
+    app.run(debug=True)  
